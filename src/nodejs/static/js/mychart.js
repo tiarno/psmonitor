649cdfc84af0e125440979fc265d5f38d4910f63
Tim Arnold <tim.arnold@sas.com>
2019-04-29 08:20:47 -0400

    updates var names

--- src/nodejs/static/js/mychart.js
+++ src/nodejs/static/js/mychart.js
@@ -1,6 +1,6 @@
 function formatBytes(bytes,decimals) {
   if(bytes == 0) return '0';
-  var k = 1024,
+  const k = 1024,
   dm = decimals <= 0 ? 0 : decimals || 2,
   sizes = ['Bytes', 'kB', 'mB', 'gB', 'tB', 'pB', 'eB', 'zB', 'yB'],
   i = Math.floor(Math.log(bytes) / Math.log(k));
